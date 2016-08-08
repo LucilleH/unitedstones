1fa6731b45e17b2a61cce610682e7970ac13bac4
LucilleH <lucille.hua@hotmail.com>
2016-08-08 13:21:45 -0400

    add constract form

--- inventory/admin.py
+++ inventory/admin.py
@@ -4,24 +4,47 @@ from inventory.models import *
 from inventory.constants import *
 
 class InventoryAdmin(admin.ModelAdmin):
-	list_display = ('product', 'current_quantity', 'quantity_status')
+	list_display = ('product', 'current_quantity', 'factory_onhold_item', 'client_onhold_item', 'resulting_quantity', 'quantity_status')
 	ordering = ['product']
-
+	search_fields = ['^product__model']
 
 class ProductAdmin(admin.ModelAdmin):
 	list_display = ('model', 'color', 'suggested_price', 'cost')
 	ordering = ['model', 'color']
 
-class InventoryOrderAdmin(admin.ModelAdmin):
-	list_display = ('manager', 'item', 'quantity', 'datetime', 'arrived')
-	ordering = ['-datetime']
+class FactoryOrderItemInline(admin.TabularInline):
+	model = FactoryOrderItem
+	extra = 5
 
+class FactoryContractAdmin(admin.ModelAdmin):
+	list_display = ('contract_number', 'manager', 'factory', 'date_created', 'total_price', 'is_all_item_delivered')
+	ordering = ['-date_created']
+	search_fields = ['^contract_number']
+	inlines = [FactoryOrderItemInline]
 
-class ItemSoldAdmin(admin.ModelAdmin):
-	list_display = ('user', 'item', 'quantity', 'datetime', 'completed')
-	ordering = ['-datetime']
+class FactoryOrderItemAdmin(admin.ModelAdmin):
+	list_display = ('contract', 'item', 'order_quantity', 'date_created', 'number_received', 'expecting', 'is_complete')
+	ordering = ['-date_created']
+	search_fields = ['^item__model']
+
+class ClientOrderItemInline(admin.TabularInline):
+	model = ClientOrderItem
+	extra = 5
+
+class ClientContractAdmin(admin.ModelAdmin):
+	list_display = ('contract_number', 'manager', 'client_name', 'date_created', 'total_price', 'is_all_item_installed')
+	ordering = ['-date_created']
+	search_fields = ['contract__client_name']
+	inlines = [ClientOrderItemInline]
+
+class ClientOrderItemAdmin(admin.ModelAdmin):
+	list_display = ('contract', 'item', 'order_quantity', 'date_created', 'number_installed', 'ongoing', 'is_complete')
+	ordering = ['-date_created']
+	search_fields = ['contract__client_name']
 
 admin.site.register(Inventory, InventoryAdmin)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(InventoryOrder, InventoryOrderAdmin)
-admin.site.register(ItemSold, ItemSoldAdmin)
+admin.site.register(FactoryContract, FactoryContractAdmin)
+admin.site.register(FactoryOrderItem, FactoryOrderItemAdmin)
+admin.site.register(ClientContract, ClientContractAdmin)
+admin.site.register(ClientOrderItem, ClientOrderItemAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -24,28 +24,89 @@ class Product(models.Model):
 	def get_name(self):
 		return self.name
 
-class InventoryOrder(models.Model):
+class FactoryContract(models.Model):
 	class Meta:
-		ordering = ['-datetime']
+		ordering = ['-date_created']
 
+	contract_number = models.CharField(max_length=32)
 	manager = models.ForeignKey(User)
-	item = models.ForeignKey(Product)
-	quantity = models.IntegerField()
-	datetime = models.DateTimeField(auto_now=True)
 	factory = models.CharField(max_length=255, choices=FACTORY, null=True, blank=True)
-	arrived = models.BooleanField(default=False)
+	total_price = models.DecimalField(max_digits=10, decimal_places=2)
+	date_created = models.DateTimeField(auto_now_add=True)
+	notes = models.TextField(null=True, blank=True)
 
-class ItemSold(models.Model):
+	def __unicode__(self):
+		return self.contract_number
+
+	def is_all_item_delivered(self):
+		f_list = FactoryOrderItem.objects.filter(contract=self)
+		if f_list.count() == 0:
+			return False
+		for l in f_list:
+			if not l.is_complete():
+				return False
+		return True
+	is_all_item_delivered.boolean = True
+
+class FactoryOrderItem(models.Model):
 	class Meta:
-		ordering = ['-datetime']
+		ordering = ['-date_created']
 
-	user = models.ForeignKey(User)
+	contract = models.ForeignKey(FactoryContract)
 	item = models.ForeignKey(Product)
-	quantity = models.IntegerField()
-	datetime = models.DateTimeField(auto_now=True)
-	price = models.DecimalField(max_digits=10, decimal_places=2)
-	status = models.IntegerField(choices=ORDERSTATUS, null=True, blank=True)
-	completed = models.BooleanField(default=False)
+	order_quantity = models.IntegerField()
+	date_created = models.DateTimeField(auto_now_add=True)
+	factory = models.CharField(max_length=255, choices=FACTORY, null=True, blank=True)
+	number_received = models.IntegerField(default=0)
+
+	def expecting(self):
+		return self.order_quantity - self.number_received
+
+	def is_complete(self):
+		return self.order_quantity == self.number_received
+	is_complete.boolean = True
+
+
+class ClientContract(models.Model):
+	class Meta:
+		ordering = ['-date_created']
+
+	contract_number = models.CharField(max_length=32)
+	manager = models.ForeignKey(User)
+	client_name = models.CharField(max_length=32)
+	total_price = models.DecimalField(max_digits=10, decimal_places=2)
+	date_created = models.DateTimeField(auto_now_add=True)
+	notes = models.TextField(null=True, blank=True)
+
+	def __unicode__(self):
+		return self.contract_number
+
+	def is_all_item_installed(self):
+		c_list = ClientOrderItem.objects.filter(contract=self)
+		if c_list.count() == 0:
+			return False
+		for l in c_list:
+			if not l.is_complete():
+				return False
+		return True
+	is_all_item_installed.boolean = True
+
+class ClientOrderItem(models.Model):
+	class Meta:
+		ordering = ['-date_created']
+
+	contract = models.ForeignKey(ClientContract)
+	item = models.ForeignKey(Product)
+	order_quantity = models.IntegerField()
+	date_created = models.DateTimeField(auto_now_add=True)
+	number_installed = models.IntegerField(default=0)
+
+	def ongoing(self):
+		return self.order_quantity - self.number_installed
+
+	def is_complete(self):
+		return self.order_quantity == self.number_installed
+	is_complete.boolean = True
 
 class Inventory(models.Model):
 
@@ -56,20 +117,47 @@ class Inventory(models.Model):
 		return self.product.model
 
 	def current_quantity(self):
-		in_list = InventoryOrder.objects.filter(item=self.product, arrived=True)
-		out_list = ItemSold.objects.filter(item=self.product, completed=True)
+		in_list = FactoryOrderItem.objects.filter(item=self.product)
+		out_list = ClientOrderItem.objects.filter(item=self.product)
 		in_quantity = 0
 		out_quantity = 0
 
 		for l in in_list:
-			in_quantity += l.quantity
+			in_quantity += l.number_received
 		for l in out_list:
-			out_quantity += l.quantity
+			out_quantity += l.number_installed
 
 		return self.start_off_quantity + in_quantity - out_quantity
 
+	def factory_onhold_item(self):
+		in_list = FactoryOrderItem.objects.filter(item=self.product)
+		in_quantity = 0
+		for l in in_list:
+			in_quantity += l.order_quantity - l.number_received
+		return in_quantity
+
+	def client_onhold_item(self):
+		out_list = ClientOrderItem.objects.filter(item=self.product)
+		out_quantity = 0
+		for l in out_list:
+			out_quantity += l.order_quantity - l.number_installed
+
+		return out_quantity
+
+	def resulting_quantity(self):
+		in_list = FactoryOrderItem.objects.filter(item=self.product)
+		out_list = ClientOrderItem.objects.filter(item=self.product)
+		in_quantity = 0
+		out_quantity = 0
+
+		for l in in_list:
+			in_quantity += l.order_quantity
+		for l in out_list:
+			out_quantity += l.order_quantity
+		return self.start_off_quantity + in_quantity - out_quantity
+
 	def quantity_status(self):
-		if self.current_quantity() > 5:
+		if self.resulting_quantity() >= 0:
 			return True
 		else:
 			return False
