536b506b8acfde565b188bcb8eae04a9a1616dd3
LucilleH <lucille.hua@hotmail.com>
2016-08-08 14:34:46 -0400

    fix settings

--- unitedstones/conf.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DB_ENGINE="django.db.backends.sqlite3"
-DB_PORT=""
-DB_HOST=""
-DB_PASSWORD=""
-DB_USER=""
-DB_NAME=os.path.join(BASE_DIR, 'db.sqlite3')
